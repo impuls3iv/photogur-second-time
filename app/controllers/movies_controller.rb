247a88790a351b729f623fc8140a92d030605d71
Impuls3iv <sammy.unterman@gmail.com>
2016-06-15 15:17:09 -0400

    refactoring code to save movies to db

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,18 @@ class MoviesController < ApplicationController
   end
 
   def create
-    render text: "Saving movie..URL: #{params[:movie][:url]}, Title #{params[:movie][:title]}, Director #{params[:movie][:director]}, Released #{params[:release]}, Image URL #{params[:url]} "
+    @movie = Movie.new(protect_params)
+    if @movie.save
+      redirect_to movies_url
+    else
+      render :new
+    end
+    # render text: "Saving movie..URL: #{params[:movie][:url]}, Title #{params[:movie][:title]}, Director #{params[:movie][:director]}, Released #{params[:release]}, Image URL #{params[:url]} "
+  end
+
+  private
+  def protect_params
+    params.require(:movie).permit(:title, :director, :release, :url )
   end
 
 end
