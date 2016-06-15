eea5e33f1c9b41a1761780110f4b5f92225334b8
Impuls3iv <sammy.unterman@gmail.com>
2016-06-15 14:30:51 -0400

    Making pictures show at /pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,32 @@
 Movie.create(
 
-  title: "Count of Monte Cristo"
-  director: "Alexandre Dumas"
-  release: "1999"
+  title: "Count of Monte Cristo",
+  director: "Alexandre Dumas",
+  release: "1999",
+  url: "http://moviehole.net/img/the-count-of-monte-cristo.jpg"
 
 )
 
 Movie.create(
 
-  title: "Black Hawk Down"
-  director: "Some dude"
-  release: "1997"
-
+  title: "Black Hawk Down",
+  director: "Some dude",
+  release: "1997",
+  url: "https://fanart.tv/api/download.php?type=download&image=49326&section=3"
 )
 
 Movie.create(
 
-  title: "Dawn of Planet of the Apes"
-  director: "A dude"
-  release: "2010"
+  title: "Dawn of Planet of the Apes",
+  director: "A dude",
+  release: "2010",
+  url: "http://blogs-images.forbes.com/markhughes/files/2014/11/Dawn-of-the-Planet-of-the-Apes-5.jpg"
 
 )
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# #
+# # Examples:
+# #
+# #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+# #   Mayor.create(name: 'Emanuel', city: cities.first)
