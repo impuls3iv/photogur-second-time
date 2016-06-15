a4e628a73932271a7c3d09c84afdd998f812c39d
Impuls3iv <sammy.unterman@gmail.com>
2016-06-15 15:06:30 -0400

    added feature to create new movie using a form...cant save yet

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,12 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    render text: "Saving movie..URL: #{params[:movie][:url]}, Title #{params[:movie][:title]}, Director #{params[:movie][:director]}, Released #{params[:release]}, Image URL #{params[:url]} "
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'movies' => 'movies#index'
-  get 'movies/:id' => 'movies#show', as: 'movie'
-  #resources :movies
+  #get 'movies' => 'movies#index'
+  #get 'movies/:id' => 'movies#show', as: 'movie'
+  root 'movies#index'
+  resources :movies
 
 
 
